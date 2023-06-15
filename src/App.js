de479db5d7ac2424bbe9058ba0b4e9ff4328ac61
Madxo <mohitsharma069@gmail.com>
2023-06-15 10:37:35 +0530

    Add user changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AddUser from './Components/Users/AddUser';
+import UsersList from './Components/Users/UsersList';
 
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  const addUser = (user) => {
+    setUsers((prevUsers) => [...prevUsers, user])
+  }
+
   return (
     <div>
-
+      <AddUser onAddUser={addUser}></AddUser>
+      <UsersList users={users}></UsersList>
     </div>
   );
 }
